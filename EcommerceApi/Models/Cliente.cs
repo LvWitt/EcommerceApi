db63851c7dc28e569ee19619cab85ca3e381dc45
LvWitt <leovitorwitt@gmail.com>
2024-06-03 22:13:05 -0300

    feat: classe de pedido e cliente

---
+++ EcommerceApi/Models/Cliente.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EcommerceApi.Models
+{
+    public class Cliente
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Nome eh obrigatorio")]
+        public string? Nome { get; set; }
+        [Required(ErrorMessage = "Cpf eh obrigatorio")]
+        public string? Cpf { get; set; }
+        [Required(ErrorMessage = "Cep eh obrigatorio")]
+        public int Cep { get; set; }
+        public int Rua { get; set; }
+        public int Cidade { get; set; }
+        public string? Numero { get; set; }
+        public string? Complemento { get; set; }
+    }
+}
